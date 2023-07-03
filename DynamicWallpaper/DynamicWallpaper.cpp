b5c48e5e467e9af94438c5d49147307ef1653058
Ethan George <codewuren@gmail.com>
2023-07-03 19:30:58 +0800

    添加 DynamicWallpaper.hpp & 修改 DynamicWallpaper.cpp 部分内容

--- DynamicWallpaper/DynamicWallpaper.cpp
+++ DynamicWallpaper/DynamicWallpaper.cpp
@@ -1,24 +1,24 @@
-﻿#include <iostream>     // 标准I/O库
-#include <Windows.h>    // Windows API
-#include <string>       // 字符串
-#include <vector>       // Vector容器
+﻿// 文件：DynamicWallpaper.cpp
+// 作者：CodeWuRen
+
+
+// 头文件
+#include <iostream>                 // 标准I/O库
+#include <Windows.h>                // Windows API
+#include <string>                   // 字符串
+#include <vector>                   // Vector容器
+#include "DynamicWallpaper.hpp"     // 必需的变量
 
 
 /// <summary>
 /// 主函数
 /// </summary>
-/// <param name="argc"></param>
-/// <param name="argv"></param>
 int main(int argc, char** argv) {
-    std::string FilePath;                       // 目标文件全路径
-    std::string Width;                          // 播放窗口长度
-    std::string Height;                         // 播放窗口宽度
-    std::vector < std::string> Arguments;       // 传给该程序的参数
-
     // 将所有参数放入Vector容器中，char** 中的元素无法直接与 char* 比较。
     for (int i = 1; i < argc; i++) {
         Arguments.push_back(argv[i]);
     }
+
     // 比较 Arguments 容器中的所有元素，获得 FilePath, Width, Height 的值。
     for (int i = 0; i < argc - 1; i++) {
         if (Arguments[i] == "-f") {
@@ -31,6 +31,8 @@ int main(int argc, char** argv) {
             Height = Arguments[i + 1];
         }
     }
+
+    // 输出相应参数值
     std::cout << "FilePath: " << FilePath << "\n" << "Width: " << Width << "\n" << "Height: " << Height << "\n";
     return 0;
 }
---
+++ DynamicWallpaper/DynamicWallpaper.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <vector>
+
+// 变量
+std::string FilePath;                       // 目标文件全路径
+std::string Width;                          // 播放窗口长度
+std::string Height;                         // 播放窗口宽度
+std::vector < std::string> Arguments;       // 传给该程序的参数
